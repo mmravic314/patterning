56ac49f0d2ad184165bb1bb9aaa748b89e5659b9
Marco Mravic <mmravic@iq218.(none)>
2016-02-21 19:46:42 -0800

    i just deleted a bunch of extra stuff

--- searchFrags/full_4strand.pds.m
+++
@@ -1,3 +0,0 @@
-3.05772 /home/xray/termanal/support.default/151218_masterDB_oPDB/4DT5.pds [(27,39), (47,60), (67,80), (91,104)] 3.05772
-3.09433 /home/xray/termanal/support.default/151218_masterDB_oPDB/4DT5.pds [(170,182), (190,203), (210,223), (234,247)] 3.09433
-3.19473 /home/xray/termanal/support.default/151218_masterDB_oPDB/1RMG.pds [(324,336), (292,305), (256,269), (228,241)] 3.19473
